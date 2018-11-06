2afb16ec8a60893ccd9a88f19641887e9edfdd3f
RISMME <python@istruly.sexy>
2018-11-06 23:31:15 +0900

    route

--- Server/view/__init__.py
+++ Server/view/__init__.py
@@ -7,4 +7,14 @@ class Router:
         self.api = Api(app)
 
     def register(self):
-        pass
+        from view.auth.signup import SignupView
+        from view.auth.login import LoginView
+        self.api.add_resource(SignupView, '/signup')
+        self.api.add_resource(LoginView, '/login')
+
+        from view.post.comment import CommentView
+        self.api.add_resource(CommentView, '/comment/<id>')
+
+        from view.post.post import PostListView, PostView
+        self.api.add_resource(PostListView, '/post_list')
+        self.api.add_resource(PostView, '/post/<postId>')
